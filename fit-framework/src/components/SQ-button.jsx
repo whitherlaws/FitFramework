7c0eb5d3ff098b7794533f7cb01246ac6114179e
whitherlaws <krispoebaikul@gmail.com>
2025-02-15 14:49:37 -0700

    Full Width button done
    Done with the buttons

--- fit-framework/src/components/SQ-button.jsx
+++ fit-framework/src/components/SQ-button.jsx
@@ -1,9 +1,7 @@
-import '../App.css'
-
 const Squarebutton = () => {
   return (
     <div className='text-center'>
-      <button className='btn p-5 text-center bg-gradient-to-r from-fitpurple-50 to-fitblue rounded-lg text-black'>Hello world</button>
+      <button className='btn p-5 text-center bg-gradient-to-r from-fitpurple-50 to-fitblue rounded-lg text-black uppercase'>Hello<br /> World</button>
     </div>
   )
 }
