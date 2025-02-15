63d9f9224b004d71cfdbe6beedd79802ebcb29b6
Rjansen2 <rapajansen14@gmail.com>
2025-02-15 16:50:10 -0700

    Header and FitFrameLogo Components added

--- fit-framework/tailwind.config.js
+++ fit-framework/tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         fitpurple: {
-          50: '#BF9ACA',
-          100: '#1E1825'
+          50: "#BF9ACA",
+          100: "#1E1825",
         },
+<<<<<<< Updated upstream
         'fitblue': '#96C5B0',
         'fitorange': '#FF875C',
       }
+=======
+        fitblue: "#96C5B0",
+        fitorange: "#FF875C",
+      },
+>>>>>>> Stashed changes
     },
   },
   plugins: [],
-}
+};
