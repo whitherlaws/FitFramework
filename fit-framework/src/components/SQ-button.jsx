3c97b0d0ade7f06efa4e481647ef54b650567f38
whitherlaws <krispoebaikul@gmail.com>
2025-02-15 13:34:07 -0700

    Update custom styles and square button
    Add custom colors in tailwind.config
    Initiate a square button

---
+++ fit-framework/src/components/SQ-button.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+const Squarebutton = () => {
+  return (
+    <div className='text-center'>
+      <button className='btn p-5 text-center bg-gradient-to-r from-fitpurple-50 to-fitblue rounded-lg text-black'>Hello world</button>
+    </div>
+  )
+}
+
+export default Squarebutton
